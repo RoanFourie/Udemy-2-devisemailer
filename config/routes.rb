b91f7b1be6b259717e52e643bbec2e917a450d7d
Roan Fourie <roanfourie@gmail.com>
2017-01-13 17:27:57 +0200

    made a mistake somewhere along with the branches

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
+
+  devise_for :users, :controllers => { registrations: 'registrations' }
   resources :articles
-  devise_for :users
   root to: 'pages#index'
 
   # If you want a localhost:3000/hello to route to the contacts page then:
   # get 'hello', to: 'pages#contact'
   get 'pages/contact'
-
   get 'pages/about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
