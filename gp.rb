a92fd79c4dc8e33bd428dcbb1ee270985281efa8
Roan Fourie <roanfourie@gmail.com>
2017-01-14 12:09:03 +0200

    From gp.rb: setup environments - production.rb

--- gp.rb
+++ gp.rb
@@ -1,5 +1,5 @@
 
 system("git add -A")
-system("git commit -m \"Automated safe\"")
+system("git commit -m \"From gp.rb: setup environments - production.rb\"")
 system("git push origin master")
 system("git push heroku master")
