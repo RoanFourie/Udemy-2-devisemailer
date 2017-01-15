45c99ecefd24dd80b643b4ba5c30dbac3e16757b
Roan Fourie <roanfourie@gmail.com>
2017-01-15 14:18:20 +0200

    Completed up to (083) Lab 18 - Updating Views

--- gp_b.rb
+++ gp_b.rb
@@ -1,6 +1,6 @@
 
 branch = "lab18"
-comments = "Completed up to (081) Lab 18 - Updating Views"
+comments = "Completed up to (083) Lab 18 - Updating Views"
 
 system("git add -A")
 system("git commit -m \"#{comments}\"")
